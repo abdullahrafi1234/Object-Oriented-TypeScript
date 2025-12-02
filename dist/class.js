70b77ae8f5f49345eb55389019b30081c71a7e0f
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-12-03 01:17:26 +0600

    install typescript

---
+++ dist/class.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Object-Oriented TypeScript");
+const objedc = {
+    id: 2555,
+    name: "abir",
+};
+//# sourceMappingURL=class.js.map
