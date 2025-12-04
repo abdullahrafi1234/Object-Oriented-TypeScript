1ad4068522f65dcff1fbdf0f3da3176ed8922206
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-12-04 12:06:37 +0600

    declare constructor using properties method

--- src/class.ts
+++ src/class.ts
@@ -1,15 +1,52 @@
-// oop - class ==> object
+// OOP - class ==> object
 
+// class Animal {
+//   name: string;
+//   species: string;
+//   sound: string;
+
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+
+//   // class ba object er bitor jokhn kono function thake tokhn take method bole
+
+//   makeSound() {
+//     console.log(`${this.name} is making sound ${this.sound}`);
+//   }
+// }
+
+// Parameter Properties
 class Animal {
-  name: string;
-  species: string;
-  sound: string;
+  // public name: string;
+  // public species: string;
+  // public sound: string;
 
-  constructor(name: string, species: string, sound: string) {
-    this.name = name;
-    this.species = species;
-    this.sound = sound;
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string
+  ) {
+    // this.name = name;
+    // this.species = species;
+    // this.sound = sound;
+  }
+
+  // class ba object er bitor jokhn kono function thake tokhn take method bole
+
+  makeSound() {
+    console.log(`${this.name} is making sound ${this.sound}`);
   }
 }
 
-const dog = new Animal("dogesh", "dog", "gheo ghew");
+const dog = new Animal("Dogesh", "dog", "gheo ghew");
+
+const cat = new Animal("Cat bhai", "cat", "mew mew");
+
+console.log(dog.sound);
+// console.log(dog.makeSound());  // eivabe dile undefined ashbe
+
+dog.makeSound();
+cat.makeSound();
