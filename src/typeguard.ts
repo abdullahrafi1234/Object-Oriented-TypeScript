d9a2f80109474c075d603543bdbcb971a9071df1
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-12-05 01:11:06 +0600

    added type guard

---
+++ src/typeguard.ts
@@ -0,0 +1,35 @@
+// type guard
+
+// in typeof
+const add = (num1: number | string, num2: number | string) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else {
+    num1.toString() + num2.toString();
+  }
+};
+
+console.log(add(2, 2));
+add("2", 2);
+add("2", "2");
+
+// in guard
+
+type NormalUser = {
+  name: string;
+};
+
+type AdminUser = {
+  name: string;
+  role: "Admin";
+};
+
+const getUserInfo = (user: NormalUser | AdminUser) => {
+  if ("role" in user) {
+    console.log(`This ${user.name} and his role is : ${user.role}`);
+  } else {
+    console.log(`${user.name}`);
+  }
+};
+
+getUserInfo({ name: "Normal", role: "Admin" });
