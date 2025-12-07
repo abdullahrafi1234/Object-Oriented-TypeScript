d6998428b1fabc560d67544df9adb718544ea209
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-12-07 12:41:12 +0600

    function guard added

--- src/instanceofgurad.ts
+++ src/instanceofgurad.ts
@@ -6,6 +6,9 @@ class Person {
   constructor(name: string) {
     this.name = name;
   }
+  getSleep(numOfHours: number) {
+    console.log(`${this.name} doinik ${numOfHours} ghonta ghumay`);
+  }
 }
 
 class Student extends Person {
@@ -14,7 +17,7 @@ class Student extends Person {
   }
 
   doStudy(numOfHours: number) {
-    console.log(`${this.name} doinik ${numOfHours} hours ghumay`);
+    console.log(`${this.name} doinik ${numOfHours} hours Pore`);
   }
 }
 
@@ -27,3 +30,42 @@ class Teacher extends Person {
     console.log(`${this.name} ${numOfHours} ti class ney`);
   }
 }
+
+// function guard
+const isStudent = (user: Person) => {
+  return user instanceof Student; // true | false
+};
+
+const isTeacher = (user: Person) => {
+  return user instanceof Teacher; // user is Teacher
+};
+
+const getUserInfo = (user: Person) => {
+  if (isStudent(user)) {
+    user.doStudy(10);
+  } else if (isTeacher(user)) {
+    user.takeClass(5);
+  } else {
+    user.getSleep(15);
+  }
+};
+// const getUserInfo = (user: Person) => {
+//   if (user instanceof Student) {
+//     user.doStudy(10);
+//   } else if (user instanceof Teacher) {
+//     user.takeClass(5);
+//   } else {
+//     user.getSleep(15);
+//   }
+// };
+
+// kono class theke banano object k instance bole
+// garir design diye onk gula gari toiri
+
+const student1 = new Student("Mr. Student");
+const teacher1 = new Teacher("Mr. Teacher");
+const person1 = new Person("Mr. Person");
+
+getUserInfo(student1);
+getUserInfo(teacher1);
+getUserInfo(person1);
