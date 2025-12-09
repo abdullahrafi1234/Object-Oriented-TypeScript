38d1fd8e3ef9d4ed6833dcd8a9ee02d4e15f4f20
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-12-09 12:30:23 +0600

    added abstraction with interface

---
+++ src/abstraction.ts
@@ -0,0 +1,33 @@
+// Abstraction, the 3rd pillar of OOP
+
+// idea
+// implementation pore korbo
+
+/**
+ * 1. interface
+ * 2. abstract class
+ */
+
+// idea
+// interface MediaPlayer {
+//   play(): void;
+//   pause(): void;
+//   stop(): void;
+// }
+
+// // implementation
+// class MusicPlayer implements MediaPlayer {
+//   play() {
+//     console.log(`Playing Music....`);
+//   }
+//   pause() {
+//     console.log(`Music paused...`);
+//   }
+//   stop() {
+//     console.log(`Music stooped ....`);
+//   }
+// }
+
+// const myPlayer1 = new MusicPlayer();
+// myPlayer1.play();
+// // myPlayer1.stop();
